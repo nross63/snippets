9a795a5028ad18fbd37913ce621a0e94011a16d0
nross63 <nross63@gmail.com>
2014-05-14 11:23:21 -0400

    Create modular.js
    basic example of Modular Pattern using Immediately-Invoked Function Expression (IIFE)

---
+++ modular.js
@@ -0,0 +1,15 @@
+var Module=(function($, m, udefined){
+      /*################################################   
+                          PRIVATE 
+      #################################################*/
+      var private = function (){
+        //do something
+      };
+      /*################################################   
+                          PUBLIC 
+      #################################################*/      
+      m.public=function(){
+        //do something
+      };
+      return m;
+})(jQuery,window.Module = window.Module || {});
